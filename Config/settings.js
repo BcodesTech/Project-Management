a5f9c0bfc07751c986a28327184b07e0af7b2bea
bcodestech <bcodestechnology@gmail.com>
2022-05-24 17:21:41 +0100

    First Commit

---
+++ Config/settings.js
@@ -0,0 +1,12 @@
+module.exports.konet  =   {
+
+    credentials   :     {
+
+        host      :   "127.0.0.1",
+        user      :   "root",
+        password  :   "umaright",
+        database  :   "openmind"
+
+      }
+
+}
